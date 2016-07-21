3e92bef14828c4768f4f39accdd01aed42c2e406
LycheeXD <lycheeexd@gmail.com>
2016-07-21 15:36:07 -0700

    [feature] initiate search popup modal

---
+++ client/public/components/searchPopup.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+class SearchPopup extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+
+    injectTapEventPlugin();
+  }
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+  render() {
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+
+      <MuiThemeProvider>
+        <RaisedButton className="red" label="New Search" onTouchTap={this.handleOpen} />
+      </MuiThemeProvider>
+      <MuiThemeProvider>
+        <Dialog
+          title="Dialog With Actions"
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+
+        </Dialog>
+      </MuiThemeProvider>
+      </div>
+    )
+  }
+}
+
+export default SearchPopup
